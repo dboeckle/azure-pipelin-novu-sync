a720f75abb2313703976c89e209015d0bcd64c58
Dominik Böckle <59045338+dboeckle@users.noreply.github.com>
2024-10-04 13:09:16 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,49 @@
-import * as core from '@actions/core'
-import axios from 'axios'
+import tl = require('azure-pipelines-task-lib/task');
+import axios from 'axios';
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-export async function run(): Promise<void> {
-  try {
-    const bridgeUrl: string = core.getInput('bridge-url')
-    const secretKey: string = core.getInput('secret-key')
-    const apiUrl: string = core.getInput('api-url')
+async function run() {
+    try {
+        const bridgeUrl: string = tl.getInput('bridge-url', true)!;
+        const secretKey: string = tl.getInput('secret-key', true)!;
+        const apiUrl: string = tl.getInput('api-url', true)!;
 
-    if (!bridgeUrl) {
-      throw new Error('bridgeUrl must be set')
+        console.log(`Bridge URL ${bridgeUrl} ...`);
+        const response = await syncState(bridgeUrl, secretKey, apiUrl);
+
+        // Set output variables
+        tl.setVariable('result', JSON.stringify(response));
+        tl.setVariable('success', 'true');
+
+        tl.setResult(tl.TaskResult.Succeeded, 'Task completed successfully');
+    } catch (error) {
+        if (error instanceof Error) {
+            tl.setResult(tl.TaskResult.Failed, error.message);
+        } else {
+            tl.setResult(tl.TaskResult.Failed, 'An unknown error occurred');
+        }
+        tl.setVariable('result', '{}');
+        tl.setVariable('success', 'false');
     }
-    if (!secretKey) {
-      throw new Error('secretKey must be set')
-    }
-    if (!apiUrl) {
-      throw new Error('apiUrl must be set')
-    }
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Bridge URL ${bridgeUrl} ...`)
-
-    const response = await syncState(bridgeUrl, secretKey, apiUrl)
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('result', response)
-    core.setOutput('success', true)
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) {
-      core.setFailed(error.message)
-    }
-    core.setOutput('result', {})
-    core.setOutput('success', false)
-  }
 }
 
-export async function syncState(
-  bridgeUrl: string,
-  secretKey: string,
-  apiUrl: string
+async function syncState(
+    bridgeUrl: string,
+    secretKey: string,
+    apiUrl: string
 ): Promise<object> {
-  const sync = await axios.post(
-    `${apiUrl}/v1/bridge/sync?source=githubAction`,
-    {
-      bridgeUrl
-    },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `ApiKey ${secretKey}`
-      }
-    }
-  )
-
-  return sync.data
+    const sync = await axios.post(
+        `${apiUrl}/v1/bridge/sync?source=azureDevOpsExtension`,
+        {
+            bridgeUrl
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `ApiKey ${secretKey}`
+            }
+        }
+    );
+    return sync.data;
 }
+
+run();
